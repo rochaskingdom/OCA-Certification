51815b2dde113c68998686a935a0f7f5fea1d531
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-07-18 01:45:48 -0300

    Comments

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -2,11 +2,36 @@ package academy.learnprogramming;
 
 public class Main {
 
+    /**
+     * This is main method.
+     *
+     * @param args command line arguments
+     */
+
     public static void main(String[] args) {
+//       priting size of arguments
         System.out.println("args-size= " + args.length);
 
+        /*
+         * priting arguments
+         * another line
+         */
         for (int i = 0; i < args.length; i++) {
             System.out.println("args[" + i + "]=" + args[i]);
         }
+
+    }
+
+    /**
+     * Calculates sum of two integers.
+     *
+     * @param a operand
+     * @param b operand
+     * @return sum
+     */
+    public static int sum(int a, int b) {
+        return a + b;
     }
 }
+
+
