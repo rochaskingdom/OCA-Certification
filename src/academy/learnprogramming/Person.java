634349c3d0a7baa63ca409fb990d019882179ceb
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-07-18 01:27:53 -0300

    Java Class Structure

---
+++ src/academy/learnprogramming/Person.java
@@ -0,0 +1,16 @@
+package academy.learnprogramming;
+
+public class Person {
+
+    private String firstName;
+    private String lasName;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
