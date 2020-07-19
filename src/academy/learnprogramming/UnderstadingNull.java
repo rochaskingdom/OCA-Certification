d1488e1e60ae4760c3247c2bb8a580b55bc73639
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-07-19 15:40:14 -0300

    Understading Null

---
+++ src/academy/learnprogramming/UnderstadingNull.java
@@ -0,0 +1,31 @@
+package academy.learnprogramming;
+
+public class UnderstadingNull {
+
+    static Object myObject = new Object();
+    static Object myOtherObject;
+
+    static String myString;
+
+    public static void main(String[] args) {
+        Object myLocalObject = new Object();
+        Object anotherLocalObject;
+
+        String name = "Java";
+        String anotherName = null;
+
+        System.out.println("myObject= " + myObject);
+        System.out.println("myOtherObject= " + myOtherObject);
+
+        System.out.println("myLocalObject= " + myLocalObject);
+
+        System.out.println("name= " + name);
+        System.out.println("anotherName= " + anotherName);
+
+        myString = "JAVA";
+
+        System.out.println("myString= " + myString);
+
+        System.out.println("myString= " + myString);
+    }
+}
